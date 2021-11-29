1fa928e6a53632931f9b6f9f83b85b52dac86366
_macrografi <cenk.ozkan@agilatek.com>
2021-11-29 16:37:36 +0300

    fix

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,33 @@
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { EMPTY, Observable, Subscription } from 'rxjs';
+import { Product } from './product';
+import { ProductService } from './product.service';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductListComponent {
+  pageTitle = 'Product List';
+  errorMessage = '';
+  categories;
+  products$ = this.productService.product$.pipe(
+    catchError((err) => {
+      this.errorMessage = err;
+      return EMPTY;
+    })
+  );
+  sub: Subscription;
+
+  constructor(private productService: ProductService) {}
+
+  onAdd(): void {
+    console.log('Not yet implemented');
+  }
+
+  onSelected(categoryId: string): void {
+    console.log('Not yet implemented');
+  }
+}
